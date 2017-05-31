efd50b9378bddcfc47fa67255400e9047b29d099
ExtraVaganzA <rodgervdheijden@hotmail.com>
2017-05-31 10:06:56 +0200

    index
    kleine aanpassingen

--- index.php
+++ index.php
@@ -141,6 +141,45 @@
         </div>
     </div>
     <!-- /.container -->
+    
+        <!-- Navigation -->
+    <nav class="navbar navbar-default hidden-xs" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+				<img class="navbar-brand" src="./img/mobileheaderlogo.png"/>
+           		
+				<a class="navbar-brand" href="./index.php"> Micropool</a>
+            </div>
+            
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="./index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="./html/team.php">Over ons</a>
+                    </li>
+                    <li>
+                        <a href="./html/vision.php">Visie</a>
+                    </li>
+                    <li>
+                        <a href="./html/contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
 
 
     <!-- jQuery -->
